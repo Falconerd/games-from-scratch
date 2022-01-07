cbc131738f07ec2c29448060fda2b1ba7d18dd11
Dylan Falconer <me@falconerd.com>
2022-01-07 12:54:57 +1100

    Add missing arguments to function signature

--- src/render/render_internal.h
+++ src/render/render_internal.h
@@ -7,7 +7,7 @@
 SDL_Window *render_init_window(float width, float height);
 void render_init_context(SDL_Window *window);
 void render_init_shaders(uint32_t *default_shader, mat4x4 projection, float width, float height);
-void render_init_quad();
+void render_init_quad(uint32_t *quad_vao, uint32_t *quad_vbo, uint32_t *quad_ebo);
 void render_init_color_texture(uint32_t *color_texture);
 
 uint32_t render_shader_create(const char *vert_path, const char *frag_path);
