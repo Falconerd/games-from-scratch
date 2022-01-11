03c246c3ad7c9a5c29644dce8160ba34ce38ea95
Dylan Falconer <me@falconerd.com>
2022-01-11 20:15:02 +1100

    add io

---
+++ src/io/io.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "io.h"
+
+char *io_file_read(const char *path) {
+    FILE *fp = fopen(path, "r");
+
+    if (!fp) {
+        printf("Cannot read file %s\n", path);
+        exit(1);
+    }
+
+    fseek(fp, 0, SEEK_END);
+
+    ssize_t length = ftell(fp);
+
+    if (length == -1L) {
+        printf("Could not assertain length of file %s\n", path);
+        return NULL;
+    }
+
+    fseek(fp, 0, SEEK_SET);
+
+    char *buffer = malloc((length + 1) * sizeof(char));
+    if (!buffer) {
+        printf("Cannot allocate file buffer for %s\n", path);
+        exit(1);
+    }
+
+    fread(buffer, sizeof(char), length, fp);
+    buffer[length] = 0;
+
+    fclose(fp);
+
+    return buffer;
+}
---
+++ src/io/io.h
@@ -0,0 +1,6 @@
+#ifndef IO_H
+#define IO_H
+
+char *io_file_read(const char *path);
+
+#endif
