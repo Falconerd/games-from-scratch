f752cf85ba5afe8b81417bc0ed10b5218758e43d
Dylan Falconer <me@falconerd.com>
2022-01-11 20:08:31 +1100

    01 complete

---
+++ src/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <glad/glad.h>
+#include <SDL2/SDL.h>
+
+int main(void) {
+    printf("Hello there!\n");
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("Could not init SDL\n");
+        exit(1);
+    }
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+
+    SDL_Window *window = SDL_CreateWindow("Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
+    if (!window) {
+        printf("Failed to init window: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    SDL_GL_CreateContext(window);
+    if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
+        printf("Failed to init GLAD\n");
+        exit(1);
+    }
+
+    printf("OpenGL Loaded\n");
+    printf("Vendor:   %s\n", glGetString(GL_VENDOR));
+    printf("Renderer: %s\n", glGetString(GL_RENDERER));
+    printf("Version:  %s\n", glGetString(GL_VERSION));
+
+    SDL_Event e;
+    int quit = 0;
+
+    while (!quit) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                quit = 1;
+            }
+        }
+    }
+
+    return 0;
+}
