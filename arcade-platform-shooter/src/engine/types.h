dd28635f01c1d11bc21521f044507c266121c87d
Dylan Falconer <me@falconerd.com>
2022-06-07 16:37:59 +1000

    add 64bit integer types

--- arcade-platform-shooter/src/engine/types.h
+++ arcade-platform-shooter/src/engine/types.h
@@ -6,10 +6,12 @@
 #define u8  uint8_t
 #define u16 uint16_t
 #define u32 uint32_t
+#define u64 uint64_t
 #define f32 float
 #define f64 double
 #define i8  int8_t
 #define i32 int32_t
+#define i64 int64_t
 
 #endif
 
